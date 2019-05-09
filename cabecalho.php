0cee225bb8938032a81bd21154575148f69ee36f
vitoriacamargo <48717189+vitoriacamargo@users.noreply.github.com>
2019-05-09 09:11:46 -0300

    Add files via upload

---
+++ cabecalho.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Minha home page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+   /* background: #aaa;*/
+  }
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1></h1>Controle loja Vit</h1>			
+  <p>Trabalhando com desenvolvimento de projetos Responsivos</p> 
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php">HOME</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="Cliente.php">Cadastro Cliente</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="vendedore.php"> Cadastro Vendedores</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="produtos.php">Produtos</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
